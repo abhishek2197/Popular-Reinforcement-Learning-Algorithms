bec349bff39b1fe7f6be1767d4021058d917d470
abhishek2197 <abhishek.aa@somaiya.edu>
2018-02-11 17:40:50 +0530

    Added critic network

--- DDPG/policy_neural_networks.py
+++ DDPG/policy_neural_networks.py
@@ -29,9 +29,40 @@ class ActorNetwork(nn.module):
 
 	def forward(self, state):
 		out_first = nn.functional.ReLu(self.firstlay(state))
-		out_second = nn.functional.ReLu(out_first)
-		out_third = nn.functional.ReLu(out_second)
-		out_final = nn.functional.tanh(out_third)
+		out_second = nn.functional.ReLu(self.secondlay(out_first))
+		out_third = nn.functional.ReLu(self.thirdlay(out_second))
+		out_final = nn.functional.tanh(self.finallay(out_third))
 
 		return out_final*aclim
 		
+class CriticNetwork(nn.module):
+	def __init__(self, stdim, acdim):
+		super(CriticNetwork, self).__init__()
+		self.stdim = stdim
+		self.acdim = acdim
+
+		self.firstlay_state = nn.Linear(self.stdim, 256)
+		self.firstlay_state.weight.data = weight_init(self.firstlay_state.weight.data.size())
+
+		self.secondlay_state = nn.Linear(256, 128)
+		self.secondlay_state.weight.data = weight_init(self.secondlay_state.weight.data.size())
+
+		self.firstlay_action = nn.Linear(acdim, 128)
+		self.firstlay_action.weight.data = weight_init(self.firstlay_action.weight.data.size())
+
+		self.firstlay_state_action = nn.Linear(256, 128)
+		self.firstlay_state_action.weight.data = weight_init(self.firstlay_state_action.weight.data.size())
+
+		self.finallay_state_action = nn.Linear(128, 1)
+		self.finallay_state_action.weight.data.uniform__(-0.005, 0.005)
+
+	def forward(self, state, action):
+		out_state_first = nn.functional.ReLu(self.firstlay_state(state))
+		out_state_second = nn.functional.ReLu(self.secondlay_state(out_state_first))
+		out_action = nn.functional.ReLu(self.firstlay_action(action))
+		state_action = torch.cat((out_state_second, out_action), dim=1)
+
+		out_state_action_first = nn.functional.ReLu(firstlay_state_action(state_action))
+		q_state_action = nn.functional.tanh(finallay_state_action(out_state_action_first))
+
+		return q_state_action
