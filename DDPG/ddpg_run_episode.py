6e05d7942e5b80398bab24ff2a69ebfb652fbb3f
abhishek2197 <abhishek.aa@somaiya.edu>
2018-02-13 16:46:52 +0530

    Added reward batch calculation

--- DDPG/ddpg_run_episode.py
+++ DDPG/ddpg_run_episode.py
@@ -8,11 +8,12 @@ import psutil
 import replay_memory
 import policy_neural_networks
 import train_networks
+from itertools import count
 
 
 
-no_of_episodes = 6000
-no_of_steps = 2000
+no_of_episodes = 5000
+no_of_steps = 5000
 
 if __name__ == '__main__':
 	env = gym.make('Walker2d-v1')
@@ -21,29 +22,35 @@ if __name__ == '__main__':
 	trainer = train_networks.Training(env.observation_space.shape[0], env.action_space.shape[0], env.action_space.high[0], mem)
 
 	
-	avg_reward = 0.0
-	sum_reward = 0
-	for i in range(no_of_episodes):
-		obs = env.reset()
-		
-		print "Episode No - "+str(i)+"" 
-		for j in range(no_of_steps):
-			#if i%100 == 0:
-			env.render()	
+	
+	for i_episode in count(1):	
+		num_episodes = 0
+		num_steps = 0
+		reward_batch = 0
+		while num_steps < 5000:
+			obs = env.reset()
+			 
+			reward_sum = 0
+			for k in range(10000):
+					#if i%100 == 0:
+				env.render()	
 			
-			state = np.float32(obs)
-			action = trainer.next_action(state)
+				state = np.float32(obs)
+				action = trainer.next_action(state)
 
-			next_obs, reward, done, ext = env.step(action)
-			sum_reward += reward
+				next_obs, reward, done, ext = env.step(action)
+				reward_sum += reward
 			
-			if done==False:
-				next_state = np.float32(next_obs)
+				if done==False:
+					next_state = np.float32(next_obs)
 				mem.add_transition(state, action, reward, np.float32(next_state))
 			
-			trainer.learn()
-			obs = next_obs
-			if done:
-				break
-		avg_reward = sum_reward/no_of_steps
-		print avg_reward		
+				trainer.learn()
+				obs = next_obs
+				if done:
+					break
+			num_episodes += 1
+			num_steps += (k-1)
+        	reward_batch += reward_sum		
+		reward_batch /= num_episodes		
+		print "Episode No - "+str(i_episode)+" "+str(reward_batch)+""		
